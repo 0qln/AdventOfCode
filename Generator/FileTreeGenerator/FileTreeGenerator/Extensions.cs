a2d170e625692dd03c84606ba0ffe27b92a58fae
Linus Nagel <129159411+0qln@users.noreply.github.com>
2023-12-01 12:59:57 +0100

    generator

---
+++ Generator/FileTreeGenerator/FileTreeGenerator/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTreeGenerator
+{
+    public static class Extensions
+    {
+        public static string? GetParent(this string dir) => Directory.GetParent(dir)?.ToString();
+        public static string? GetParent(this DirectoryInfo dir) => Directory.GetParent(dir.ToString())?.ToString();
+    }
+}
---
+++ Generator/FileTreeGenerator/FileTreeGenerator/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace FileTreeGenerator
+{
+    class Program()
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("For what year would you like the file tree to be generated?");
+            string year = Console.ReadLine() ?? "";
+
+            // Create year dir
+            string yearDir = Path.Combine(GetAOC() ?? throw new ArgumentException(), year);
+            Directory.CreateDirectory(yearDir);
+
+            // Create the day dirs
+            CreatePathsFor(1, 25, GetDay, yearDir, partDir =>
+
+            // Create the parts of the day
+            CreatePathsFor(1, 2, GetPart, partDir));
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Create a range of directories.
+        /// </summary>
+        /// <param name="start">Inclusive.</param>
+        /// <param name="end">Inclusive.</param>
+        /// <param name="formatter">Formatter for the name of the newly created directories.</param>
+        /// <param name="location">The location that the new directories will be created in.</param>
+        /// <param name="iterationCallback">An optional callback that will be invoced after each creted directory.</param>
+        static void CreatePathsFor(int start, int end, Func<int, string> formatter, string location, Action<string>? iterationCallback = null)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                string dir = Path.Combine(location, formatter(i));
+                Directory.CreateDirectory(dir);
+                iterationCallback?.Invoke(dir);
+            }
+        }
+
+        static string GetPart(int part) => "Part_" + part.ToString();
+
+        static string GetDay(int day) => "Day_" + day.ToString();
+
+        static string? GetAOC()
+        {
+            var aocDir = Environment.CurrentDirectory;
+            for (int i = 0; i < 6; i++) aocDir = aocDir?.GetParent();
+            return aocDir;
+        }
+    }
+}
