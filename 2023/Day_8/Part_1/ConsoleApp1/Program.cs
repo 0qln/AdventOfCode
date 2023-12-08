4d65c552e13d64ba6be134ea9cacefe6921fa2a5
Linus Nagel <129159411+0qln@users.noreply.github.com>
2023-12-08 15:35:02 +0100

    day 8 part 1
    ugly solution :(

--- 2023/Day_8/Part_1/ConsoleApp1/Program.cs
+++ 2023/Day_8/Part_1/ConsoleApp1/Program.cs
@@ -2,45 +2,64 @@
 
 
 var data = File.ReadAllText(@"../../../input.txt").Split("\r\n\r\n");
-var instructions = data[0];
-var map = data[1].Split('\n');
+Console.WriteLine(AllInOneBcChallangeGoofy(data[0], data[1].Split("\n")));
 
-Console.WriteLine(TraverseTree(new Instructions(instructions), BuildTree(map)));
+//Console.WriteLine(
+//    TraverseTree(
+//        BuildTree(data[1].Split('\n'), new Instructions(data[0]))));
 
 
-Node BuildTree(string[] lines)
+int AllInOneBcChallangeGoofy(string instructions, string[] lines)
 {
-    Dictionary<string, (string left, string right)> nodes = new(); 
-    foreach (var line in lines)    
+    // Build Tree
+    Dictionary<string, (string left, string right)> nodes = new();
+    foreach (var line in lines)
         nodes.Add(NodeName(line), (LeftNode(line), RightNode(line)));
 
-    return CreateNode(nodes, NodeName(lines[0]));
-}
-
-Node CreateNode(Dictionary<string, (string left, string right)> nodes, string name)
-{
-    Console.WriteLine(name);
-    if (name == "ZZZ")
-    {
-        return new Node(name, null, null);
-    }
-    else
-    {
-        return new Node(name, 
-            CreateNode(nodes, nodes[name].left), 
-            CreateNode(nodes, nodes[name].right));
-    }
-}
-
-int TraverseTree(Instructions instructions, Node head)
-{
-    Node? current = head;
+    string currentNode = "AAA";
+    int currentInstruction = 0;
     int steps = 0;
-    while (current is not null && head.Name != "ZZZ")
+    while (currentNode != "ZZZ")
     {
-        current = current.NextChild(instructions.Next());
+        var left = instructions[currentInstruction] == 'L';
+        currentNode = left ? nodes[currentNode].left : nodes[currentNode].right;
+
+        currentInstruction++;
+        currentInstruction %= instructions.Length;
+
         steps++;
     }
+
+    return steps;
+}
+
+
+Node BuildTree(string[] lines, Instructions instructions)
+{
+    Dictionary<string, (string left, string right)> nodes = new(); 
+    foreach (var line in lines) 
+        nodes.Add(NodeName(line), (LeftNode(line), RightNode(line)));
+
+    return CreateNode(nodes, NodeName(lines[0]), instructions);
+}
+
+Node CreateNode(Dictionary<string, (string left, string right)> nodes, string name, Instructions instructions)
+{
+    if (name == "ZZZ")
+    {
+        return new Node(name, null);
+    }
+    var nextNode = instructions.Next() ? nodes[name].left : nodes[name].right;
+    return new Node(name, CreateNode(nodes, nextNode, instructions));    
+}
+
+int TraverseTree(Node head)
+{
+    Node? current = head;
+    int steps = 1;
+    while ((current = current?.NextChild()) is not null && current.Name != "ZZZ")
+        steps++;
+   
     return steps;
 }
 
@@ -49,20 +68,21 @@ string NodeName(string node) => Regex.Match(node, @"^[A-Z]+(?= )").Value;
 string LeftNode(string node) => Regex.Match(node, @"(?<=\()[A-Z]+(?=,)").Value;
 string RightNode(string node) => Regex.Match(node, @"(?<=, )[A-Z]+(?=\))").Value;
 
-record class Node(string Name, Node? Left, Node? Right)
+record class Node(string Name, Node? Next)
 {
-    public Node? NextChild(bool left) => left ? Left : Right;
+    public Node? NextChild() => Next;
 }
 
 
 record class Instructions(string Raw)
 {
-    private int _idx;
+    private int _idx = 0;
 
     /// <returns>Left</returns>
     public bool Next()
     {
-        if (++_idx == Raw.Length) _idx = 0;
-        return Raw[_idx] == 'R';
+        _idx++;
+        _idx %= Raw.Length;
+        return Raw[_idx] == 'L';
     }
 }
