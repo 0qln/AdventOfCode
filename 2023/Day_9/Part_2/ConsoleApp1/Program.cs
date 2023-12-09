db944451fc9a4436959475dc7e8119ae5cdf74fa
Linus Nagel <129159411+0qln@users.noreply.github.com>
2023-12-09 15:44:25 +0100

    day 8 part 2

---
+++ 2023/Day_9/Part_2/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+using History = long[];
+
+
+var reNum = new Regex(@"[\d-]+");
+var histories = File
+    .ReadAllLines(@"../../../input.txt")
+    .Select(x => reNum.Matches(x).Select(x => long.Parse(x.Value)).ToArray());
+
+
+long results = histories.Sum(history =>
+{
+    History original = (History)history.Clone();
+    List<long> begins = new();
+
+    do
+    {
+        begins.Add(history[0]);
+        history = Project(history, (x, y) => y - x).ToArray();
+    }
+    while (history.Any(x => x != 0));
+
+    begins.Add(0);
+
+    var newBegins = new long[begins.Count];
+
+    newBegins[0] = 0;
+
+    for (int i = 1; i < newBegins.Length; i++)
+    {
+        newBegins[i] = begins[^i] - newBegins[i - 1];
+    }
+
+    return begins[0] - newBegins[^1];
+});
+
+IEnumerable<long> Project(IEnumerable<long> sequence, Func<long, long, long> func)
+{
+    var nums = sequence.ToList();
+    for (int i = 0; i < nums.Count - 1; i++)
+    {
+        yield return func(nums[i], nums[i + 1]);
+    }
+}
+
+
+Console.WriteLine(results);
